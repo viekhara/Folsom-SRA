149d0cd1d97f51b8487c17dff2d8222cc8c3153f
viekhara <viekhara@berkeley.edu>
2026-06-11 14:06:10 -0700

    add markers

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ const map = new mapboxgl.Map({
   center: [-122.27, 37.87], // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 9 // starting zoom
     });
+map.on('load', function() {
+    map.addSource('points-data', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/viekhara/Folsom-SRA/refs/heads/main/data.geojson'
+    });
+map.addLayer({
+        id: 'points-layer',
+        type: 'circle',
+        source: 'points-data',
+        paint: {
+            'circle-color': '#4264FB',
+            'circle-radius': 6,
+            'circle-stroke-width': 2,
+            'circle-stroke-color': '#ffffff'
+        }
+    });
+});
